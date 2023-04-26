3eb7fb5e663dcffdd92485cfaac3836ae1e63f9b
causemx <allen.cause@gmail.com>
2023-04-27 00:53:27 +0800

    Reconstruct slide generator.

--- gpt2ppt/gpt2ppt.py
+++ gpt2ppt/gpt2ppt.py
@@ -4,11 +4,11 @@ import requests
 import json
 import gpt2ppt.extension as extension
 from pptx import Presentation as pt
-from .parse import parse
 
 openai.api_key = extension.OPENAI_API_KEY
 
-url='https://api.openai.com/v1/chat/completions'
+MODEL_ENGINE = "gpt-3.5-turbo"
+
 
 class GenerationError(Exception):
     def __init__(self, f, *args):
@@ -16,14 +16,47 @@ class GenerationError(Exception):
         self.f = f
 
     def __str__(self):
-        return f'There are something wrong when generating'
+        return 'There are something wrong when generating'
 
-def analyze_res(data):
-    # byte to string
-    data_str = data.decode('utf-8')
-    json_data = json.loads(data_str)
-    response = json_data['choices'][0]['message']['content']
-    return response
+class Slide:
+    _instance = None
+    SLD_LAYOUT_TITLE_AND_CONTENT = 1
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self, input=None, output=None) -> None:
+        self.prs = pt()
+        self.slide = []
+        self.entry = []
+    
+    def add(self, title, prompt):
+        slide = self.prs.slides.add_slide(
+            self.prs.slide_layouts[self.SLD_LAYOUT_TITLE_AND_CONTENT])
+        shapes = slide.shapes
+
+        title_shape = shapes.title
+        body_shape = shapes.placeholders[1]
+        title_shape.text = title
+        tf = body_shape.text_frame
+        
+        if isinstance(prompt, list):
+            for p in prompt:
+                self.add_para(tf, p)
+        else:
+            self.add_para(tf, p)
+    
+    def add_para(self, tf, content, level=1):
+        p = tf.add_paragraph()
+        p.text = content
+        p.level = level
+
+    def save(self):
+        self.prs.save('output.pptx')
+
+        
 
 def add_slide(prs, layout, title, content):
     """Return slide newly added to `prs` using `layout` and having `title`."""
@@ -40,50 +73,17 @@ def generate(args):
     output_file = args.output_file
     output = "./output.pptx" if not output_file else output_file
 
-    title_slide_layout = prs.slide_layouts[1]
-    # IMPORTANT: set slogan for starting
-    page_index = 1
-    prompt = ''
-    print("Hello")
     try:
         with open(input_file) as topo_file:
-            parse(topo_file)
-            
-        """with open(input_file) as topo_file:
             for line in topo_file:
-                print(f"generating slide page: {page_index}")
-                question, topic = line.split(',')
-                # prompt += input(":")+'\n'
-                prompt += question + 'in 50 words\n'
-                payload = {
-                    "model": "gpt-3.5-turbo",
-                    "messages": [{"role": "user", "content": prompt}]
-                    #"n": 10,
-                    #"temperature: 0.7
-                }
-                headers = {
-                    "Authorization": f"Bearer {openai.api_key}",
-                    "Content-Type":"application/json"
-                } 
-                r = requests.post(url, data=json.dumps(payload), headers=headers)
-                try:
-                    res = analyze_res(r.content)   
-                    prompt +=  res+'\n'
-                    add_slide(prs, title_slide_layout, topic, res)
-                except GenerationError:
-                    print("error:",r.content)
-                    return 1
-                
-                page_index = page_index + 1
-
-        prs.save(output)       """
+                print(line)
     except FileNotFoundError as fe:
         print("error:", str(fe))
         return 1
-
     return 0
 
-
-    
-
-    
+def foo(input):
+    response = openai.ChatCompletion.create(
+        model=MODEL_ENGINE, 
+        messages=[{"role": "user", "content": input }])   
+    return response['choices'][0]['message']['content']
